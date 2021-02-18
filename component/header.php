72d37cfff70bf15f538fd038b372656eb07a2d5f
RickiE <rickie.leehao@gmail.com>
2021-02-18 13:23:06 +0800

    meeting update

--- component/header.php
+++ component/header.php
@@ -7,7 +7,7 @@
 <div>
     <header>
         <div id="logo">
-            <img src="../img/web_iconMain.png" alt="footwearLogo" class="center">
+            <img src="./img/web_iconMain.png" alt="footwearLogo" class="center">
         </div>
         <nav>
             <ul class="navLinks">
@@ -15,7 +15,7 @@
                 <li><a href="#">Women</a></li>
                 <li><a href="#">Kids</a></li>
                 <li><a href="#">Sales</a></li>
-                <li id="cart"><a href="#"><img src="../img/Shopping-Cart-icon.png" alt="Cart"></a></li>
+                <li id="cart"><a href="#"><img src="./img/Shopping-Cart-icon.png" alt="Cart"></a></li>
             </ul>
         </nav>
     </header>
