cc8cfd51f0f5491d65e2c93449f2bce1f2ab7062
RickiE <rickie.leehao@gmail.com>
2021-02-25 20:37:35 +0800

    Meeting update

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./styles/rickieTest.css">
+    <link rel="stylesheet" href="./styles/rickie.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Newsreader&display=swap" rel="stylesheet">
