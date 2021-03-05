14587ac23f81c82384f9b9d1ef5b89a0b3529187
zexinbeh <zexinbeh@gmail.com>
2021-03-05 23:02:04 +0800

    Beh
    Img and little adjustments; add admin.php and .css

--- contact.php
+++ contact.php
@@ -5,12 +5,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./styles/rickie.css">
+    <link rel="stylesheet" href="./styles/components.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=PT+Sans&display=swap" rel="stylesheet">
 
     <link rel = "stylesheet" href = "./styles/contact.css">
+    <link rel="icon" href="./img/web_iconMain.png" type="image/x-icon">
     <title>Contact Us</title>
 </head>
 
@@ -21,10 +22,10 @@
 <div class="contactus-main" style="margin-left:50px">
     <div class="left">
         <h1 class="contactus-heading">Contact Us</h1>
-        <li>If you have any enquires, please don’t hesitate to contact us. You’ll hear from us shortly. Thank you for your patience.</li>
+        <p>If you have any enquires, please don’t hesitate to contact us. You’ll hear from us shortly. Thank you for your patience.</p>
         <br/>
 
-        <form action = "/message_submitted.php">
+        <form>
             <label for = "email">E-mail:<br/></label>
             <input type = "text" id = "email" name = "email" placeholder = "Enter your E- mail here.."> 
             <br/>
@@ -35,11 +36,11 @@
         </form>
     </div>
 
-    <div class="right">
+    <div class="right" style = "margin-top: 3em">
     
         <div class = "contactus-container">
             <div style = "text-align:right">
-                <img style = "margin: 0 0 15px 0;" src = "./img/Contact Us/Email Icon.png" width="100" height="75">
+                <img style = "margin: 0 0 15px 0;" src = "./img/Contact Us/Email Icon.png" width="75" height="50">
             </div>
                 <p class = "right"><b>salesnservices@footwear.com</b><br/>
                 We'll reply within one business day<br/>
@@ -48,8 +49,8 @@
 
         <div class = "contactus-container">
         <div style = "text-align:right">
-            <img style = "margin: 0 0 15px 0;" src = "./img/Contact Us/Smartphone Icon.png" width="100" height="100">
-            <img style = "margin: 0 0 15px 0;" src = "./img/Contact Us/Whatsapp Icon.png" width="100" height="100">
+            <img style = "margin: 0 0 15px 0;" src = "./img/Contact Us/Smartphone Icon.png" width="75" height="75">
+            <img style = "margin: 0 0 15px 0;" src = "./img/Contact Us/Whatsapp Icon.png" width="75" height="75">
         </div>
             <p class = "right"><b>012-345 6789</b><br/>
             24 hours a day, 7 days a week<br/>
@@ -58,7 +59,7 @@
 
         <div class = "contactus-container">
             <div style = "text-align:right">
-                <img style = "margin: 0 0 15px 0;" src = "./img/Contact Us/Pinpoint Icon.png" width="100" height="100">
+                <img style = "margin: 0 0 15px 0;" src = "./img/Contact Us/Pinpoint Icon.png" width="70" height="70">
             </div>
                 <p class = "right"><b>Lot 1-23, Jalan Bukit Bintang, 55100,<br/>
                 Kuala Lumpur, Wilayah Persekutuan<br/>
