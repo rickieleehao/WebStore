d28048cc936a45b336a32c1187a472c0dece1c6d
zexinbeh <zexinbeh@gmail.com>
2021-03-17 23:08:56 +0800

    .jpg converted & done basic admin page UI

--- admin.php
+++ admin.php
@@ -10,15 +10,45 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=PT+Sans&display=swap" rel="stylesheet">
 
-    <link rel = "stylesheet" href = "./styles/aboutus.css">
+    <link rel = "stylesheet" href = "./styles/admin.css">
     <link rel="icon" href="./img/web_iconMain.png" type="image/x-icon">
-    <title>About Us</title>
+    <title>Admin</title>
 </head>
 
 <body>
     <?php include "./component/header.php"; ?>
     <?php include "./component/navigator.php"; ?>
-    <?php include "./component/footer.php"; ?>
+
+        <div class = "button_grp">
+        <ul class = "nobullet">
+            <label>Category</label>
+            <li class= "button">Men</li>
+            <li class= "button">Women</li>
+            <li class= "button">Kids</li><br>
+        <ul class = "nobullet">
+            <label>Number</label>
+            <li class= "button">1</li>
+            <li class= "button">2</li>
+            <li class= "button">3</li>
+            <li class= "button">4</li>
+            <li class= "button">5</li>
+            <li class= "button">6</li>
+            <li class= "button">7</li>
+            <li class= "button">8</li>
+            <li class= "button">9</li>
+            <li class= "button">10</li>
+        </ul>
+        </div>
+
+        <h1>Administrator Page</h1><br/>
+        <form class="formclass">
+            <input type= "text" id= "name" name= "name" placeholder= "Name">
+            <input type= "text" id= "price" name= "price" placeholder= "Price">
+            <input type= "text" id= "promoRate" name= "promoRate" placeholder= "Promotion Rate">
+            <input type="submit" value="Update">
+        </form>
+        
+
     
 </body>
 
