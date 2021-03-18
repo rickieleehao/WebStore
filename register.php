dbac01052ded6174e88df4aa84bd8c45f3bb2835
Koh Jun Dong <KohComputer@gmail.com>
2021-03-18 23:31:40 +0800

    Rough completion of register component
    To note:
    - Passwords are hashed, and thus need to be verified with password_verify() function
    
    To-do:
    - Feedback for successful registration
    - Feedback for password too long (72 chars)
    - CSS issues
    - Sessions (probably handled by the login side of things?)

--- register.php
+++ register.php
@@ -1,41 +1,49 @@
 <!DOCTYPE html>
+
 <head>
-    <title>Register</title>
-    <link rel="stylesheet" href="./styles/register.css">
-    <link rel="stylesheet" href="./styles/components.css">
-    <link rel="icon" href="./img/web_iconMain.png" type="image/x-icon">
+  <title>Register</title>
+  <link rel="stylesheet" href="./styles/register.css">
+  <link rel="stylesheet" href="./styles/components.css">
+  <link rel="icon" href="./img/web_iconMain.png" type="image/x-icon">
 </head>
+
 <body>
-<h2>Become a member</h2>
-<?php include "./component/header.php"; ?>
-<?php include "./component/footer.php"; ?>
-<div id="ID">
-  <form class="register-content animate" action="/index.php" method="post">
-    <div class="imgcontainer">
-      <img src="./img/web_icon3.png" alt="Avatar" class="avatar" style="width:150px;height:150px;">
-    </div>
+  <iframe name="dummyframe" id="dummyframe" style="display: none;"></iframe>
+  <h2>Become a member</h2>
+  <!-- <php include "./component/header.php"; ?> -->
+  <!-- <php include "./component/footer.php"; ?> -->
+  <div id="ID">
+    <form class="register-content animate" action="registerhandler.php" target="dummyframe" method="post">
+      <div class="imgcontainer">
+        <img src="./img/web_icon3.png" alt="Avatar" class="avatar" style="width:150px;height:150px;">
+      </div>
 
-    <div class="container">
+      <div class="container">
         <label>Email</label>
-        <input type="email" required>
-    </div>
-    <div class="container">
+        <input type="email" name="email" required>
+      </div>
+      <div class="container">
         <label>Name</label>
-        <input type="text" required>
-    </div>
-    <div class="container">
+        <input type="text" name="name" required>
+      </div>
+      <div class="container">
         <label>Password</label>
-        <input type="password" required>
-    </div>
-    <div class="dropdown">
-      <select>
-        <option>Male</option>
-        <option>Female</option>
-      </select>
-    </div>
+        <input type="password" name="password" autocomplete="new-password" maxlength="72" required>
+      </div>
+      <div class="container">
+        <label>Retype Password</label>
+        <input type="password" name="retype-password" autocomplete="new-password" maxlength="72" required>
+      </div>
+      <div class="dropdown">
+        <select name="gender">
+          <option value="M">Male</option>
+          <option value="F">Female</option>
+        </select>
+      </div>
 
-    <button type="submit">Register</button>
-  </form>
-</div>
+      <button type="submit" name="register">Register</button>
+    </form>
+  </div>
 </body>
+
 </html>
---
+++ registerhandler.php
@@ -0,0 +1,29 @@
+<?php
+
+$sql = mysqli_connect('localhost', 'root', '', 'data');
+
+if ($sql->connect_error) {
+    die("Connection failed: " . $sql->connect_error);
+}
+
+if (isset($_POST['register'])) {
+    $register = $sql->prepare("INSERT INTO member (email,password,name,gender,type) VALUES (?,?,?,?,'Member')");
+    $register->bind_param("ssss", $email, $password, $name, $gender);
+
+    $email = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $name = $_POST['name'];
+    $gender = $_POST['gender'];
+
+    $checkemail = "SELECT email FROM member WHERE email='$email'";
+    $result = $sql->query($checkemail);
+
+    if (strcmp($_POST['password'], $_POST['retype-password']) !== 0) {
+        echo '<script>alert("Password typed is not the same")</script>';
+    } else if ($result->num_rows == 1) {
+        echo '<script>alert("Email has been registered before!")</script>';
+    } else {
+        $register->execute();
+        echo 'Register successful, but show in some form';
+    }
+}
