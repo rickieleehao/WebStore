c8aca787a92871b473f5bb95a74fd3de824a8f3c
zexinbeh <zexinbeh@gmail.com>
2021-03-28 14:04:21 +0800

    add adminheader

--- admin.php
+++ admin.php
@@ -16,39 +16,16 @@
 </head>
 
 <body>
-    <?php include "./component/header.php"; ?>
+    <?php include "./component/adminheader.php"; ?>
     <?php include "./component/navigator.php"; ?>
 
         <div class = "button_grp">
         <ul class = "nobullet">
             <label>Category</label>
-            <li class= "button">Men</li>
-            <li class= "button">Women</li>
-            <li class= "button">Kids</li><br>
-        <ul class = "nobullet">
-            <label>Number</label>
-            <li class= "button">1</li>
-            <li class= "button">2</li>
-            <li class= "button">3</li>
-            <li class= "button">4</li>
-            <li class= "button">5</li>
-            <li class= "button">6</li>
-            <li class= "button">7</li>
-            <li class= "button">8</li>
-            <li class= "button">9</li>
-            <li class= "button">10</li>
+            <li class= "button">Product</li>
+            <li class= "button">FAQ</li><br/>
         </ul>
         </div>
-
-        <h1>Administrator Page</h1><br/>
-        <form class="formclass">
-            <input type= "text" id= "name" name= "name" placeholder= "Name">
-            <input type= "text" id= "price" name= "price" placeholder= "Price">
-            <input type= "text" id= "promoRate" name= "promoRate" placeholder= "Promotion Rate">
-            <input type="submit" value="Update">
-        </form>
-        
-
     
 </body>
 
---
+++ component/adminheader.php
@@ -0,0 +1,6 @@
+<div class="headerWrapper">
+    <div class="header">
+        <img id="logo" src="./img/web_iconMain.png" alt="footwearLogo">
+        <h1>Administrator Page</h1>
+    </div>
+</div>
