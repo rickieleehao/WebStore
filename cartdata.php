cf7ca9dbbdd5830572e38ac9104e65ea1cc55d21
Koh Jun Dong <KohComputer@gmail.com>
2021-04-11 23:12:33 +0800

    Emergency bug fix
    - Cart css has now been fixed
    - Item page has some errors in grammars

--- cartdata.php
+++ cartdata.php
@@ -1,73 +1,77 @@
 <?php
-if(!isset($_SESSION['loggedin'])){
+if (!isset($_SESSION['loggedin'])) {
     echo '<script type="text/javascript"> alert("Required login to view the cart or add product.") </script>';
     header("refresh:0; url=index.php");
 }
 
 $totalPrice = 0;
 
-if(isset($_GET['action'])){
-    if($_GET['action'] == "delete"){
-        foreach($_SESSION['cart'] as $key=>$value){
-            if($value['item_id'] == $_GET['id']){
+if (isset($_GET['action'])) {
+    if ($_GET['action'] == "delete") {
+        foreach ($_SESSION['cart'] as $key => $value) {
+            if ($value['item_id'] == $_GET['id']) {
                 unset($_SESSION['cart'][$key]);
             }
         }
-    }   
-}
-
-if(isset($_POST['addcart'])&&isset($_SESSION['loggedin'])){
-    if(!isset($_POST['size'])){
-        echo '<script type="text/javascript"> alert("You need to pick a shoes size.") </script>';
-        header("refresh:0; url=shopping.php?category=All&sub=");
-    }else{
-    if(isset($_SESSION['cart'])){
-        $count = count($_SESSION['cart']);
-        $item_array = array (
-            'item_id' => $_POST['item_id'],
-            'item_name' => $_POST['item_name'],
-            'item_price' => $_POST['item_price'],
-            'item_size' => $_POST['size']
-        );
-        $_SESSION['cart'][$count] = $item_array;
-    }else{
-        $item_array = array (
-            'item_id' => $_POST['item_id'],
-            'item_name' => $_POST['item_name'],
-            'item_price' => $_POST['item_price'],
-            'item_size' => $_POST['size']
-        );
-        $_SESSION['cart'][0] = $item_array;
     }
 }
+
+if (isset($_POST['addcart']) && isset($_SESSION['loggedin'])) {
+    if (!isset($_POST['size'])) {
+        echo '<script type="text/javascript"> alert("You need to pick a shoes size.") </script>';
+        header("refresh:0; url=shopping.php?category=All&sub=");
+    } else {
+        if (isset($_SESSION['cart'])) {
+            $count = count($_SESSION['cart']);
+            $item_array = array(
+                'item_id' => $_POST['item_id'],
+                'item_name' => $_POST['item_name'],
+                'item_price' => $_POST['item_price'],
+                'item_size' => $_POST['size']
+            );
+            $_SESSION['cart'][$count] = $item_array;
+        } else {
+            $item_array = array(
+                'item_id' => $_POST['item_id'],
+                'item_name' => $_POST['item_name'],
+                'item_price' => $_POST['item_price'],
+                'item_size' => $_POST['size']
+            );
+            $_SESSION['cart'][0] = $item_array;
+        }
+    }
 }
 
 $num = 1;
-if(isset($_SESSION['cart'])){
+if (isset($_SESSION['cart'])) {
     echo "<div class='cartWrapper'>";
-    foreach($_SESSION['cart'] as $key=>$value){
-            echo "<div class='card'>";
-            echo "<div class='card left'>";
-            echo "<h1>".$num."</h1>";
-            echo "<img src='./img/Shoes/".$value['item_id']."_1.jpg'>";
-            echo "</div>";
-            echo "<div class='card right'>";
-            echo "<h1>".$value['item_name']."</h1>";
-            echo "<p class='price'>RM ".$value['item_price']."</p>";
-            echo "<p class='price'>Size ".$value['item_size']."</p>";
-            echo "<a href='cart.php?action=delete&id=".$value['item_id']."'>Remove</a>";
-            echo "</div>";
-            echo "</div>";
-            $totalPrice += $value['item_price'];
-            $num+=1;
-        }
-        echo "</div>";
+    foreach ($_SESSION['cart'] as $key => $value) {
+        echo <<<HTML
+            <div class='card'>
+                <div class='card left'>
+                    <h1>$num</h1>
+                    <img src='./img/Shoes/{$value["item_id"]}_1.jpg'>
+                </div>
+                <div class='card right'>
+                    <p class='item_name'>{$value["item_name"]}</p>
+                    <p class='price'>RM {$value['item_price']}</p>
+                    <p class='price'>Size - {$value['item_size']}</p>
+                    <a href='cart.php?action=delete&id={$value['item_id']}'>Remove</a>
+                </div>
+            </div>
+            <hr>
+            HTML;
+        $totalPrice += $value['item_price'];
+        $num += 1;
     }
-echo "<div class='summary'>";
-echo "<div class='summaryWrapper'>";
-echo "<h1>Summary</h1>";
-echo "<h1>Total Price ".$totalPrice."</h1>";
-echo "<button>Make Payment</button>";
-echo "</div>";
-echo "</div>";
-?>
+    echo "</div>";
+}
+echo <<<HTML
+<div class='summary'>
+    <div class='summaryWrapper'>
+        <h1>Summary</h1>
+        <p>Total Price - RM $totalPrice</p>
+        <button>Make Payment</button>
+    </div>
+</div>
+HTML;
--- itemPage.php
+++ itemPage.php
@@ -1,17 +1,17 @@
 <?php
 include_once "configdb.php";
-$shoes=$_GET['shoes'];
+$shoes = $_GET['shoes'];
 
 $query = "SELECT * FROM shoes_data WHERE id = '$shoes'";
-if ($result = mysqli_query($conn,$query)){
-    while($obj = mysqli_fetch_object($result)){
-    $id = "$obj->id";  
-    $category = "$obj->sub_category";
-    $name = $obj->name;
-    $price = $obj->price;
-    $itemDesc = $obj->product_description;
-    $colorDesc = $obj->color_description;
-}
+if ($result = mysqli_query($conn, $query)) {
+    while ($obj = mysqli_fetch_object($result)) {
+        $id = "$obj->id";
+        $category = "$obj->sub_category";
+        $name = $obj->name;
+        $price = $obj->price;
+        $itemDesc = $obj->product_description;
+        $colorDesc = $obj->color_description;
+    }
 }
 
 mysqli_close($conn);
@@ -39,50 +39,55 @@ mysqli_close($conn);
         <div class="itemImg">
             <!-- php for img -->
             <div>
-                <img class="Img Img-1" src="./img/Shoes/<?php echo $shoes?>_1.jpg">
-                <img class="Img Img-2" src="./img/Shoes/<?php echo $shoes?>_2.jpg">
-                <img class="Img Img-3" src="./img/Shoes/<?php echo $shoes?>_3.jpg">
-                <img class="Img Img-4" src="./img/Shoes/<?php echo $shoes?>_4.jpg">
+                <img class="Img Img-1" src="./img/Shoes/<?php echo $shoes ?>_1.jpg">
+                <img class="Img Img-2" src="./img/Shoes/<?php echo $shoes ?>_2.jpg">
+                <img class="Img Img-3" src="./img/Shoes/<?php echo $shoes ?>_3.jpg">
+                <img class="Img Img-4" src="./img/Shoes/<?php echo $shoes ?>_4.jpg">
             </div>
         </div>
         <div class="itemDets">
             <div>
                 <!-- php for text -->
                 <div class="itemDetsWrapper">
-                    <p class="category"><?php echo $category?></p>
-                    <p class="name"><?php echo $name?></p>
+                    <p class="category"><?php echo $category ?></p>
+                    <p class="name"><?php echo $name ?></p>
                     <p class="size">Select Size</p>
-                    <p class="price">RM <?php echo $price?></p>
+                    <p class="price">RM <?php echo $price ?></p>
                 </div>
                 <div class="addCart">
                     <form action="cart.php" method="POST">
                         <div class="sizePicker">
                             <div><input type="radio" name="size" value="US 7">
-                                <label>US 7</label></div>
+                                <label>US 7</label>
+                            </div>
                             <div><input type="radio" name="size" value="US 8">
-                                <label>US 8</label></div>
+                                <label>US 8</label>
+                            </div>
                             <div><input type="radio" name="size" value="US 9">
-                                <label>US 9</label></div>
+                                <label>US 9</label>
+                            </div>
                             <div><input type="radio" name="size" value="US 10">
-                                <label>US 10</label></div>
+                                <label>US 10</label>
+                            </div>
                             <div><input type="radio" name="size" value="US 11">
-                                <label>US 11</label></div>
+                                <label>US 11</label>
+                            </div>
                             <div><input type="radio" name="size" value="US 12">
-                                <label>US 12</label></div>
+                                <label>US 12</label>
+                            </div>
                         </div>
-                        <input type="hidden" name="item_id" value="<?php echo $id?>">
-                        <input type="hidden" name="item_name" value="<?php echo $name?>">
-                        <input type="hidden" name="item_price" value="<?php echo $price?>">
+                        <input type="hidden" name="item_id" value="<?php echo $id ?>">
+                        <input type="hidden" name="item_name" value="<?php echo $name ?>">
+                        <input type="hidden" name="item_price" value="<?php echo $price ?>">
                         <input type="hidden" name="addcart" value="">
                         <button type="submit">Add to Cart</button>
                     </form>
                 </div>
-                <p class="itemDesc"><?php echo $itemDesc?></p>
+                <p class="itemDesc"><?php echo $itemDesc ?></p>
 
-                <p class="itemColors">- <?php echo $colorDesc?></p>
+                <p class="itemColors">- <?php echo $colorDesc ?></p>
                 <br><br><br><br>
-                <p class="returnPolicy">No return allowed on any Final Sale items,except for it flaws in workmanship are
-                    found (as long as the hang tag has naot been removed)</p>
+                <p class="returnPolicy">No return allowed on any Final Sale items, except for flaws in workmanship (as long as the hang tag has not been removed.</p>
 
             </div>
         </div>
